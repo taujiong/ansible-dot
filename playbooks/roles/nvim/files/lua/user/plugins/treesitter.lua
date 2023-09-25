a0da6652ffe0b2bd9a76557a8583c93cb1884f20
taujiong <taujiong@outlook.com>
2023-09-25 20:55:41 +0800

    feat(nvim): add plugin treesitter as syntax highlighter

---
+++ playbooks/roles/nvim/files/lua/user/plugins/treesitter.lua
@@ -0,0 +1,35 @@
+---@type LazySpec
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = "VeryLazy",
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		"windwp/nvim-ts-autotag",
+	},
+	---@type TSConfig
+	---@diagnostic disable-next-line: missing-fields
+	opts = {
+		auto_install = true,
+		autotag = { enable = true },
+		highlight = { enable = true },
+		indent = { enable = true },
+		context_commentstring = {
+			enable = true,
+			enable_autocmd = false,
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-=>",
+				node_incremental = "<C-=>",
+				scope_incremental = "<nop>",
+				node_decremental = "<C-->",
+			},
+		},
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
+	end,
+}
