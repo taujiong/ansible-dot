6fac8c9e7c4acc8de5c6dd78ecf2fb1911cabd74
taujiong <taujiong@outlook.com>
2023-09-26 12:02:03 +0800

    refactor(nvim): update keymaps

--- playbooks/roles/nvim/files/lua/user/mappings.lua
+++ playbooks/roles/nvim/files/lua/user/mappings.lua
@@ -2,14 +2,43 @@ local wk = require("which-key")
 local icons = require("user.icons")
 
 wk.register({
+	j = { "v:count == 0 ? 'gj' : 'j'", "Move cursor down", expr = true },
+	k = { "v:count == 0 ? 'gk' : 'k'", "Move cursor up", expr = true },
+	x = { '"_x', "Cut char without copy" },
+	xx = { '"_dd', "Cut line without copy" },
+	gx = { require("user.utils").open_with_system, "Open with system app" },
+	["\\"] = { "<cmd>split<cr>", "Split horizontally" },
+	["|"] = { "<cmd>vsplit<cr>", "Split vertically" },
+	["<m-j>"] = { "<cmd>m .+1<cr>==", "Move line down" },
+	["<m-k>"] = { "<cmd>m .-2<cr>==", "Move line up" },
+	["<c-h>"] = { "<c-w>h", "Go to left window" },
+	["<c-j>"] = { "<c-w>j", "Go to lower window" },
+	["<c-k>"] = { "<c-w>k", "Go to upper window" },
+	["<c-l>"] = { "<c-w>l", "Go to right window" },
+	["<c-s>"] = { "<cmd>w<cr>", "Save file" },
+	["<esc>"] = { "<cmd>noh<esc>", "Clear highlight" },
+})
+
+wk.register({
+	jk = { "<esc>", "Back to normal mode", mode = "i" },
+	["<m-j>"] = { "<esc><cmd>m .+1<cr>==gi", "Move line down" },
+	["<m-k>"] = { "<esc><cmd>m .-2<cr>==gi", "Move line up" },
+	["<c-s>"] = { "<esc><cmd>w<cr>==gi", "Save file" },
+}, { mode = "i" })
+
+wk.register({
 	e = { "<cmd>NvimTreeToggle<cr>", "Toggle explorer" },
+	q = { "<cmd>q<cr>", "Quit" },
 }, { prefix = "<leader>" })
 
 wk.register({
 	p = {
 		name = icons.Package .. " Packages",
-		m = { "<cmd>Mason<cr>", "Mason status" },
-		s = { "<cmd>Lazy<cr>", "Plugins status" },
+		m = { "<cmd>Mason<cr>", "Show mason packages" },
+		s = { "<cmd>Lazy<cr>", "Show lazy plugins" },
+		c = { "<cmd>Neoconf<cr>", "Update local/global neoconf" },
+		l = { "<cmd>Neoconf lsp<cr>", "Show lsp settings" },
+		n = { "<cmd>Neoconf show<cr>", "Show merged neoconf" },
 	},
 }, { prefix = "<leader>" })
 
@@ -17,7 +46,7 @@ wk.register({
 	l = {
 		name = icons.ActiveLSP .. " Lsp",
 		g = { "<cmd>LspLog<cr>", "Show lsp log" },
-		i = { "<cmd>LspInfo<cr>", "Lsp information" },
-		m = { "<cmd>LspRestart<cr>", "Lsp restart" },
+		i = { "<cmd>LspInfo<cr>", "Show lsp information" },
+		m = { "<cmd>LspRestart<cr>", "Restart lsp" },
 	},
 }, { prefix = "<leader>" })
