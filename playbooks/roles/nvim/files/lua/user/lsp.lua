16708a32d53b4c6dce92843fcfdce15bdd865286
taujiong <taujiong@outlook.com>
2023-09-27 12:41:41 +0800

    feat(nvim): add more lsp features

--- playbooks/roles/nvim/files/lua/user/lsp.lua
+++ playbooks/roles/nvim/files/lua/user/lsp.lua
@@ -1,62 +1,225 @@
 local M = {}
 
----@param bufnr number
-local on_lsp_attach = function(_, bufnr)
-	local opts = { buffer = bufnr }
-	vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-	vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-	vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-	vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-	vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-	vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-	vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-	vim.keymap.set("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-	vim.keymap.set({ "n", "x" }, "<leader>lf", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-	vim.keymap.set("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+function M.has_capability(capability, filter)
+  for _, client in ipairs(vim.lsp.get_active_clients(filter)) do
+    if client.supports_method(capability) then
+      return true
+    end
+  end
+end
 
-	vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
-	vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
-	vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
+local function add_buffer_autocmd(augroup, bufnr, autocmds)
+  if not vim.tbl_islist(autocmds) then
+    autocmds = { autocmds }
+  end
+  local cmds_found, cmds = pcall(vim.api.nvim_get_autocmds, { group = augroup, buffer = bufnr })
+  if not cmds_found or vim.tbl_isempty(cmds) then
+    vim.api.nvim_create_augroup(augroup, { clear = false })
+    for _, autocmd in ipairs(autocmds) do
+      local events = autocmd.events
+      autocmd.events = nil
+      autocmd.group = augroup
+      autocmd.buffer = bufnr
+      vim.api.nvim_create_autocmd(events, autocmd)
+    end
+  end
+end
+
+local function del_buffer_autocmd(augroup, bufnr)
+  local cmds_found, cmds = pcall(vim.api.nvim_get_autocmds, { group = augroup, buffer = bufnr })
+  if cmds_found then
+    vim.tbl_map(function(cmd)
+      vim.api.nvim_del_autocmd(cmd.id)
+    end, cmds)
+  end
+end
+
+local format = function(bufnr)
+  vim.lsp.buf.format({
+    bufnr = bufnr,
+    timeout_ms = 5000,
+  })
+end
+
+---@param bufnr number
+local on_lsp_attach = function(client, bufnr)
+  local wk = require("which-key")
+  wk.register({
+    ["[d"] = { vim.diagnostic.goto_prev, "Previous diagnostic" },
+    ["]d"] = { vim.diagnostic.goto_next, "Next diagnostic" },
+    ["gl"] = { vim.diagnostic.open_float, "Diagnostic on current line" },
+    ["<leader>fd"] = { require("telescope.builtin").diagnostics, "Find diagnostics" },
+    ["<leader>li"] = { "<cmd>LspInfo<cr>", "Show lsp information" },
+  }, { buffer = bufnr })
+
+  if client.supports_method("textDocument/codeAction") then
+    wk.register({
+      ["<leader>la"] = { vim.lsp.code_action, "Show lsp code actions" },
+    }, { mode = { "n", "v" }, buffer = bufnr })
+  end
+
+  if client.supports_method("textDocument/codeLens") then
+    local augroup_name = "user_lsp_codelens_refresh"
+    add_buffer_autocmd(augroup_name, bufnr, {
+      events = { "InsertLeave", "BufEnter" },
+      desc = "Refresh lsp codelens",
+      callback = function()
+        if not M.has_capability("textDocument/codeLens", { bufnr = bufnr }) then
+          del_buffer_autocmd(augroup_name, bufnr)
+          return
+        end
+        vim.lsp.codelens.refresh()
+      end,
+    })
+    wk.register({
+      l = {
+        l = { vim.lsp.codelens.refresh, "Refresh lsp codelens" },
+        L = { vim.lsp.codelens.run, "Run lsp codelens" },
+      },
+    }, { prefix = "<leader>", buffer = bufnr })
+  end
+
+  if client.supports_method("textDocument/declaration") then
+    wk.register({
+      gD = { vim.lsp.buf.declaration, "Declaration of current symbol" },
+    }, { buffer = bufnr })
+  end
+
+  if client.supports_method("textDocument/definition") then
+    wk.register({
+      gd = { require("telescope.builtin").lsp_definitions, "Show the definition of current symbol" },
+    }, { buffer = bufnr })
+  end
+
+  if client.supports_method("textDocument/typeDefinition") then
+    wk.register({
+      gD = { require("telescope.builtin").lsp_type_definitions, "Definition of current type" },
+    })
+  end
+
+  if client.supports_method("textDocument/hover") then
+    -- TODO: Remove mapping after dropping support for Neovim v0.9, it's automatic
+    wk.register({
+      K = { vim.lsp.buf.hover, "Hover symbol details" },
+    })
+  end
+
+  if client.supports_method("textDocument/implementation") then
+    wk.register({
+      gI = { require("telescope.builtin").lsp_implementations, "Implementation of current symbol" },
+    })
+  end
+
+  if client.supports_method("textDocument/inlayHint") then
+    if vim.lsp.inlay_hint then
+      vim.lsp.inlay_hint(bufnr, true)
+    end
+  end
+
+  if client.supports_method("textDocument/references") then
+    wk.register({
+      gr = { require("telescope.builtin").lsp_references, "References of current symbol" },
+    })
+  end
+
+  if client.supports_method("textDocument/rename") then
+    wk.register({
+      ["<leader>lr"] = { vim.lsp.buf.rename, "Rename current symbol" },
+    })
+  end
+
+  if client.supports_method("textDocument/signatureHelp") then
+    wk.register({
+      ["<leader>lh"] = { vim.lsp.buf.signature_help, "Show signature help" },
+    })
+  end
+
+  if client.supports_method("textDocument/formatting") then
+    local augroup_name = "user_lsp_format"
+    add_buffer_autocmd(augroup_name, bufnr, {
+      events = "BufWritePre",
+      desc = "Autoformat on save",
+      callback = function()
+        if not M.has_capability("textDocument/formatting", { bufnr = bufnr }) then
+          del_buffer_autocmd(augroup_name, bufnr)
+        end
+        format(bufnr)
+      end,
+    })
+    wk.register({
+      lf = {
+        function()
+          format(bufnr)
+        end,
+        "Format with lsp",
+      },
+    }, { prefix = "<leader>", mode = { "n", "v" }, buffer = bufnr })
+  end
+
+  if client.supports_method("textDocument/documentHighlight") then
+    local augroup_name = "user_lsp_document_highlight"
+    add_buffer_autocmd(augroup_name, bufnr, {
+      {
+        events = { "CursorHold", "CursorHoldI" },
+        desc = "Highlight references when cursor holds",
+        callback = function()
+          if not M.has_capability("textDocument/documentHighlight", { bufnr = bufnr }) then
+            del_buffer_autocmd(augroup_name, bufnr)
+            return
+          end
+          vim.lsp.buf.document_highlight()
+        end,
+      },
+      {
+        events = { "CursorMoved", "CursorMovedI", "BufLeave" },
+        desc = "Clear references when cursor moves",
+        callback = function()
+          vim.lsp.buf.clear_references()
+        end,
+      },
+    })
+  end
+end
+
+local get_server_lsp_opts = function(server_name)
+  local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+  local server_settings = {}
+  if server_name == "jsonls" then
+    local schemastore = require("schemastore")
+    server_settings = {
+      json = {
+        schemas = schemastore.json.schemas(),
+        validate = { enable = true },
+      },
+    }
+  elseif server_name == "yamlls" then
+    local schemastore = require("schemastore")
+    server_settings = {
+      yaml = {
+        schemas = schemastore.yaml.schemas(),
+      },
+    }
+  end
+
+  return {
+    capabilities = cmp_capabilities,
+    settings = server_settings,
+    on_attach = on_lsp_attach,
+  }
 end
 
 local setup_server_handler = function()
-	local lspconfig = require("lspconfig")
-	local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-	require("mason-lspconfig").setup_handlers({
-		function(server_name)
-			lspconfig[server_name].setup({
-				capabilities = cmp_capabilities,
-				on_attach = on_lsp_attach,
-			})
-		end,
-		jsonls = function(server_name)
-			local schemastore = require("schemastore")
-			lspconfig[server_name].setup({
-				capabilities = cmp_capabilities,
-				settings = {
-					json = {
-						schemas = schemastore.json.schemas(),
-						validate = { enable = true },
-					},
-				},
-			})
-		end,
-		yamlls = function(server_name)
-			local schemastore = require("schemastore")
-			lspconfig[server_name].setup({
-				capabilities = cmp_capabilities,
-				settings = {
-					yaml = {
-						schemas = schemastore.yaml.schemas(),
-					},
-				},
-			})
-		end,
-	})
+  local lspconfig = require("lspconfig")
+  require("mason-lspconfig").setup_handlers({
+    function(server_name)
+      local server_opts = get_server_lsp_opts(server_name)
+      lspconfig[server_name].setup(server_opts)
+    end,
+  })
 end
 
 function M.setup()
-	setup_server_handler()
+  setup_server_handler()
 end
 
 return M
--- playbooks/roles/nvim/files/lua/user/mappings.lua
+++ playbooks/roles/nvim/files/lua/user/mappings.lua
@@ -2,158 +2,157 @@ local wk = require("which-key")
 local icons = require("user.icons")
 
 wk.register({
-	j = { "v:count == 0 ? 'gj' : 'j'", "Move cursor down", expr = true },
-	k = { "v:count == 0 ? 'gk' : 'k'", "Move cursor up", expr = true },
-	x = { '"_x', "Cut char without copy" },
-	xx = { '"_dd', "Cut line without copy" },
-	gx = { require("user.utils").open_with_system, "Open with system app" },
-	["\\"] = { "<cmd>split<cr>", "Split horizontally" },
-	["|"] = { "<cmd>vsplit<cr>", "Split vertically" },
-	["<m-j>"] = { "<cmd>m .+1<cr>==", "Move line down" },
-	["<m-k>"] = { "<cmd>m .-2<cr>==", "Move line up" },
-	["<c-h>"] = { "<c-w>h", "Go to left window" },
-	["<c-j>"] = { "<c-w>j", "Go to lower window" },
-	["<c-k>"] = { "<c-w>k", "Go to upper window" },
-	["<c-l>"] = { "<c-w>l", "Go to right window" },
-	["<c-s>"] = { "<cmd>w<cr>", "Save file" },
-	["<c-/>"] = {
-		function()
-			require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
-		end,
-		"Toggle comment line",
-	},
-	["<esc>"] = { "<cmd>noh<esc>", "Clear HIGhlight" },
+  j = { "v:count == 0 ? 'gj' : 'j'", "Move cursor down", expr = true },
+  k = { "v:count == 0 ? 'gk' : 'k'", "Move cursor up", expr = true },
+  x = { '"_x', "Cut char without copy" },
+  xx = { '"_dd', "Cut line without copy" },
+  gx = { require("user.utils").open_with_system, "Open with system app" },
+  ["\\"] = { "<cmd>split<cr>", "Split horizontally" },
+  ["|"] = { "<cmd>vsplit<cr>", "Split vertically" },
+  ["<m-j>"] = { "<cmd>m .+1<cr>==", "Move line down" },
+  ["<m-k>"] = { "<cmd>m .-2<cr>==", "Move line up" },
+  ["<c-h>"] = { "<c-w>h", "Go to left window" },
+  ["<c-j>"] = { "<c-w>j", "Go to lower window" },
+  ["<c-k>"] = { "<c-w>k", "Go to upper window" },
+  ["<c-l>"] = { "<c-w>l", "Go to right window" },
+  ["<c-s>"] = { "<cmd>w<cr>", "Save file" },
+  ["<c-/>"] = {
+    function()
+      require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
+    end,
+    "Toggle comment line",
+  },
+  ["<esc>"] = { "<cmd>noh<esc>", "Clear HIGhlight" },
 })
 
 wk.register({
-	jk = { "<esc>", "Back to normal mode", mode = "i" },
-	["<m-j>"] = { "<esc><cmd>m .+1<cr>==gi", "Move line down" },
-	["<m-k>"] = { "<esc><cmd>m .-2<cr>==gi", "Move line up" },
-	["<c-s>"] = { "<esc><cmd>w<cr>==gi", "Save file" },
+  jk = { "<esc>", "Back to normal mode", mode = "i" },
+  ["<m-j>"] = { "<esc><cmd>m .+1<cr>==gi", "Move line down" },
+  ["<m-k>"] = { "<esc><cmd>m .-2<cr>==gi", "Move line up" },
+  ["<c-s>"] = { "<esc><cmd>w<cr>==gi", "Save file" },
 }, { mode = "i" })
 
 wk.register({
-	["<c-/>"] = {
-		"<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
-		"Toggle comment for selection",
-	},
+  ["<c-/>"] = {
+    "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+    "Toggle comment for selection",
+  },
 }, { mode = "v" })
 
 wk.register({
-	e = { require("nvim-tree.api").tree.toggle, "Toggle explorer" },
-	q = { "<cmd>q<cr>", "Quit" },
-	i = { desc = "Toggle cursor word" },
+  e = { require("nvim-tree.api").tree.toggle, "Toggle explorer" },
+  q = { "<cmd>q<cr>", "Quit" },
+  i = { desc = "Toggle cursor word" },
 }, { prefix = "<leader>" })
 
 wk.register({
-	dm = {
-		name = "marks",
-		l = { require("marks").delete_line, "Delete marks on current line" },
-		f = { require("marks").delete_buf, "Delete marks on current file" },
-	},
-	m = {
-		name = "Marks",
-		m = { require("marks").toggle, "Toggle mark" },
-		["["] = { require("marks").prev, "Go to previous mark" },
-		["]"] = { require("marks").next, "Go to next mark" },
-	},
+  dm = {
+    name = "marks",
+    l = { require("marks").delete_line, "Delete marks on current line" },
+    f = { require("marks").delete_buf, "Delete marks on current file" },
+  },
+  m = {
+    name = "Marks",
+    m = { require("marks").toggle, "Toggle mark" },
+    ["["] = { require("marks").prev, "Go to previous mark" },
+    ["]"] = { require("marks").next, "Go to next mark" },
+  },
 })
 
 wk.register({
-	s = { require("substitute").operator, "Replace with {motion}" },
-	ss = { require("substitute").line, "Replace with line" },
-	p = { require("substitute").visual, "Replace in visual", { mode = "v" } },
-	sx = { require("substitute.exchange").operator, "Exchange with {motion}" },
-	sxx = { require("substitute.exchange").line, "Exchange with line" },
-	sxc = { require("substitute.exchange").cancel, "Exchange exchange" },
-	X = { require("substitute.exchange").visual, "Exchange in visual", { mode = "v" } },
+  s = { require("substitute").operator, "Replace with {motion}" },
+  ss = { require("substitute").line, "Replace with line" },
+  p = { require("substitute").visual, "Replace in visual", { mode = "v" } },
+  sx = { require("substitute.exchange").operator, "Exchange with {motion}" },
+  sxx = { require("substitute.exchange").line, "Exchange with line" },
+  sxc = { require("substitute.exchange").cancel, "Exchange exchange" },
+  X = { require("substitute.exchange").visual, "Exchange in visual", { mode = "v" } },
 })
 
 wk.register({
-	z = {
-		R = { require("ufo").openAllFolds, "Open all folds" },
-		M = { require("ufo").closeAllFolds, "Close all folds" },
-		r = { require("ufo").openFoldsExceptKinds, "Fold less" },
-		m = { require("ufo").closeFoldsWith, "Fold more" },
-		p = { require("ufo").peekFoldedLinesUnderCursor, "Peek fold" },
-	},
+  z = {
+    R = { require("ufo").openAllFolds, "Open all folds" },
+    M = { require("ufo").closeAllFolds, "Close all folds" },
+    r = { require("ufo").openFoldsExceptKinds, "Fold less" },
+    m = { require("ufo").closeFoldsWith, "Fold more" },
+    p = { require("ufo").peekFoldedLinesUnderCursor, "Peek fold" },
+  },
 })
 
 wk.register({
-	["]g"] = { require("gitsigns").next_hunk, "Next git hunk", { prefix = "" } },
-	["[g"] = { require("gitsigns").prev_hunk, "Previous git hunk", { prefix = "" } },
-	g = {
-		name = icons.Git .. " Git",
-		l = { require("gitsigns").blame_line, "View git blame" },
-		p = { require("gitsigns").preview_hunk, "Preview git hunk" },
-		h = { require("gitsigns").reset_hunk, "Reset git hunk" },
-		H = { require("gitsigns").reset_buffer, "Reset git buffer" },
-		s = { require("gitsigns").stage_hunk, "Stage git hunk" },
-		S = { require("gitsigns").state_buffer, "Stage git buffer" },
-		u = { require("gitsigns").undo_stage_hunk, "Unstage git buffer" },
-		d = { require("gitsigns").diffthis, "View git diff" },
-		g = { require("neogit").open, "Open git" },
-		b = {
-			function()
-				require("telescope.builtin").git_branches({ use_file_path = true })
-			end,
-			"Show git branches",
-		},
-		c = {
-			function()
-				require("telescope.builtin").git_commits({ use_file_path = true })
-			end,
-			"Show git commits for current repository",
-		},
-		C = {
-			function()
-				require("telescope.builtin").git_bcommits({ use_file_path = true })
-			end,
-			"Show git commits for current buffer",
-		},
-	},
+  ["]g"] = { require("gitsigns").next_hunk, "Next git hunk", { prefix = "" } },
+  ["[g"] = { require("gitsigns").prev_hunk, "Previous git hunk", { prefix = "" } },
+  g = {
+    name = icons.Git .. " Git",
+    l = { require("gitsigns").blame_line, "View git blame" },
+    p = { require("gitsigns").preview_hunk, "Preview git hunk" },
+    h = { require("gitsigns").reset_hunk, "Reset git hunk" },
+    H = { require("gitsigns").reset_buffer, "Reset git buffer" },
+    s = { require("gitsigns").stage_hunk, "Stage git hunk" },
+    S = { require("gitsigns").state_buffer, "Stage git buffer" },
+    u = { require("gitsigns").undo_stage_hunk, "Unstage git buffer" },
+    d = { require("gitsigns").diffthis, "View git diff" },
+    g = { require("neogit").open, "Open git" },
+    b = {
+      function()
+        require("telescope.builtin").git_branches({ use_file_path = true })
+      end,
+      "Show git branches",
+    },
+    c = {
+      function()
+        require("telescope.builtin").git_commits({ use_file_path = true })
+      end,
+      "Show git commits for current repository",
+    },
+    C = {
+      function()
+        require("telescope.builtin").git_bcommits({ use_file_path = true })
+      end,
+      "Show git commits for current buffer",
+    },
+  },
 }, { prefix = "<leader>" })
 
 wk.register({
-	p = {
-		name = icons.Package .. " Packages",
-		m = { "<cmd>Mason<cr>", "Show mason packages" },
-		s = { "<cmd>Lazy<cr>", "Show lazy plugins" },
-		c = { "<cmd>Neoconf<cr>", "Update local/global neoconf" },
-		l = { "<cmd>Neoconf lsp<cr>", "Show lsp settings" },
-		n = { "<cmd>Neoconf show<cr>", "Show merged neoconf" },
-	},
+  p = {
+    name = icons.Package .. " Packages",
+    m = { "<cmd>Mason<cr>", "Show mason packages" },
+    s = { "<cmd>Lazy<cr>", "Show lazy plugins" },
+    c = { "<cmd>Neoconf<cr>", "Update local/global neoconf" },
+    l = { "<cmd>Neoconf lsp<cr>", "Show lsp settings" },
+    n = { "<cmd>Neoconf show<cr>", "Show merged neoconf" },
+  },
 }, { prefix = "<leader>" })
 
 wk.register({
-	l = {
-		name = icons.ActiveLSP .. " Lsp",
-		g = { "<cmd>LspLog<cr>", "Show lsp log" },
-		i = { "<cmd>LspInfo<cr>", "Show lsp information" },
-		m = { "<cmd>LspRestart<cr>", "Restart lsp" },
-	},
+  l = {
+    name = icons.ActiveLSP .. " Lsp",
+    g = { "<cmd>LspLog<cr>", "Show lsp log" },
+    m = { "<cmd>LspRestart<cr>", "Restart lsp" },
+  },
 }, { prefix = "<leader>" })
 
 wk.register({
-	f = {
-		name = icons.Search .. " Find",
-		["<cr>"] = { require("telescope.builtin").resume, "Resume previous search" },
-		["'"] = { require("telescope.builtin").marks, "Find marks" },
-		w = { require("telescope.builtin").live_grep, "Find words" },
-		c = { require("telescope.builtin").grep_string, "Find word under cursor" },
-		C = { require("telescope.builtin").commands, "Find commands" },
-		f = { require("telescope.builtin").find_files, "Find files" },
-		F = {
-			function()
-				require("telescope.builtin").find_files({ hidden = true, no_ignore = true })
-			end,
-			"Find all files",
-		},
-		h = { require("telescope.builtin").help_tags, "Find help" },
-		m = { require("telescope.builtin").man_pages, "Find man" },
-		n = { "<cmd>Noice telescope<cr>", "Find notifications" },
-		o = { require("telescope.builtin").oldfiles, "Find history" },
-		r = { require("telescope.builtin").registers, "Find registers" },
-		p = { require("telescope.builtin").builtin, "Find all pickers" },
-	},
+  f = {
+    name = icons.Search .. " Find",
+    ["<cr>"] = { require("telescope.builtin").resume, "Resume previous search" },
+    ["'"] = { require("telescope.builtin").marks, "Find marks" },
+    w = { require("telescope.builtin").live_grep, "Find words" },
+    c = { require("telescope.builtin").grep_string, "Find word under cursor" },
+    C = { require("telescope.builtin").commands, "Find commands" },
+    f = { require("telescope.builtin").find_files, "Find files" },
+    F = {
+      function()
+        require("telescope.builtin").find_files({ hidden = true, no_ignore = true })
+      end,
+      "Find all files",
+    },
+    h = { require("telescope.builtin").help_tags, "Find help" },
+    m = { require("telescope.builtin").man_pages, "Find man" },
+    n = { "<cmd>Noice telescope<cr>", "Find notifications" },
+    o = { require("telescope.builtin").oldfiles, "Find history" },
+    r = { require("telescope.builtin").registers, "Find registers" },
+    p = { require("telescope.builtin").builtin, "Find all pickers" },
+  },
 }, { prefix = "<leader>" })
