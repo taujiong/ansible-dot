955d74f6932185a58dc7e1cf6e2469e52361d6c6
taujiong <taujiong@outlook.com>
2023-11-04 13:29:30 +0800

    feat: add hammerspoon spoons

--- files/hammerspoon/init.lua
+++ files/hammerspoon/init.lua
@@ -1,23 +1,6 @@
 hs.loadSpoon("SpoonInstall")
 spoon.SpoonInstall:andUse("EmmyLua")
-
-local chooser = hs.chooser.new(function(selectedItem)
-  hs.alert.show(hs.inspect(selectedItem))
-end)
-chooser:choices({
-  {
-    ["text"] = "First Choice",
-    ["subText"] = "This is the subtext of the first choice",
-    ["uuid"] = "0001",
-  },
-  { ["text"] = "Second Option", ["subText"] = "I wonder what I should type here?", ["uuid"] = "Bbbb" },
-  {
-    ["text"] = hs.styledtext.new(
-      "Third Possibility",
-      { font = { size = 18 }, color = hs.drawing.color.definedCollections.hammerspoon.green }
-    ),
-    ["subText"] = "What a lot of choosing there is going on here!",
-    ["uuid"] = "III3",
-  },
+spoon.SpoonInstall:andUse("ReloadConfiguration", {
+  start = true,
 })
-chooser:show()
+hs.alert("Hammerspoon started")
