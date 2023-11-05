a3d97b1ceeee468dda3632409b27d34b5a5c25ad
taujiong <taujiong@outlook.com>
2023-11-05 10:36:36 +0800

    feat: update hammerspoon

--- files/hammerspoon/Spoons/EmmyLua.spoon/init.lua
+++ files/hammerspoon/Spoons/EmmyLua.spoon/init.lua
@@ -1,35 +1,7 @@
---- === EmmyLua ===
----
---- Thie plugin generates EmmyLua annotations for Hammerspoon and any installed Spoons
---- under ~/.hammerspoon/Spoons/EmmyLua.spoon/annotations.
---- Annotations will only be generated if they don't exist yet or are out of date.
----
---- Note: Load this Spoon before any pathwatchers are defined to avoid unintended behaviour (for example multiple reloads when the annotions are created).
----
---- In order to get auto completion in your editor, you need to have one of the following LSP servers properly configured:
---- * [lua-language-server](https://github.com/sumneko/lua-language-server) (recommended)
---- * [EmmyLua-LanguageServer](https://github.com/EmmyLua/EmmyLua-LanguageServer)
----
---- To start using this annotations library, add the annotations folder to your workspace.
---- for lua-languag-server:
----
---- ```json
---- {
----   "Lua.workspace.library": ["/Users/YOUR_USERNAME/.hammerspoon/Spoons/EmmyLua.spoon/annotations"]
---- }
---- ```
----
---- Download: [https://github.com/Hammerspoon/Spoons/raw/master/Spoons/EmmyLua.spoon.zip](https://github.com/Hammerspoon/Spoons/raw/master/Spoons/EmmyLua.spoon.zip)
-
 local M = {}
 
-M.name = "EmmyLua"
-M.version = "1.0"
-M.author = "http://github.com/folke"
-M.license = "MIT - https://opensource.org/licenses/MIT"
-
 local options = {
-  annotations = hs.spoons.resourcePath("annotations"),
+  annotations = hs.spoons.resourcePath("../../annotations"),
   types = {
     bool = "boolean",
     boolean = "boolean",
@@ -48,8 +20,6 @@ local options = {
     size = "hs.geometry",
   },
 }
-
-M.spoonPath = hs.spoons.scriptPath()
 
 function M.comment(str, commentStr)
   commentStr = commentStr or "--"
@@ -199,14 +169,6 @@ function M:init()
   hs.fs.mkdir(options.annotations)
   -- Load hammerspoon docs
   M.create(hs.docstrings_json_file)
-
-  -- Load Spoons
-  for _, spoon in ipairs(hs.spoons.list()) do
-    local doc = hs.configdir .. "/Spoons/" .. spoon.name .. ".spoon/docs.json"
-    if hs.fs.attributes(doc, "modification") then
-      M.create(doc, "spoon.")
-    end
-  end
 end
 
 return M
--- files/hammerspoon/init.lua
+++ files/hammerspoon/init.lua
@@ -1,6 +1,1 @@
-hs.loadSpoon("SpoonInstall")
-spoon.SpoonInstall:andUse("EmmyLua")
-spoon.SpoonInstall:andUse("ReloadConfiguration", {
-  start = true,
-})
-hs.alert("Hammerspoon started")
+hs.loadSpoon("EmmyLua")
